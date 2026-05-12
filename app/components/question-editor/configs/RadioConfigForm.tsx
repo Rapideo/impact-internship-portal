f27dd3215cf585b7e7c61c0df69144cfd247849b
Matthew Smith <matthew.smith@rapideo.com>
2026-05-11 23:15:38 -0400

    feat(qe): add RadioConfigForm

---
+++ app/components/question-editor/configs/RadioConfigForm.tsx
@@ -0,0 +1,35 @@
+import { OptionsListEditor } from '../OptionsListEditor';
+import type { RadioQuestion as Q } from '../../../lib/question-types';
+
+interface Props {
+  question: Q;
+  onChange: (next: Q) => void;
+}
+
+export function RadioConfigForm({ question, onChange }: Props) {
+  return (
+    <>
+      <OptionsListEditor
+        options={question.config.options}
+        onChange={(opts) =>
+          onChange({ ...question, config: { ...question.config, options: opts } })
+        }
+      />
+      <div className="field" style={{ marginTop: 12 }}>
+        <label>
+          <input
+            type="checkbox"
+            checked={question.config.otherWithText}
+            onChange={(e) =>
+              onChange({
+                ...question,
+                config: { ...question.config, otherWithText: e.target.checked },
+              })
+            }
+          />{' '}
+          Allow &quot;Other&quot; with text reveal
+        </label>
+      </div>
+    </>
+  );
+}
